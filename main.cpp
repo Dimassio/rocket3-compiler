3c2124e680fee9be1945390276dc371445e22e4e
Demidov Alexander <demidov-study@yandex.ru>
2015-09-29 14:19:42 +0300

    Added file input

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
+#include <cstdio>
 extern "C" int yylex();
 
-int main()
-{
+int main(int argc, char *argv[]) {
+	if (argc < 2) {
+		printf("Please, add <input file> parameter\n");
+	}
+
+	extern FILE *yyin;
+	yyin = fopen(argv[1], "r");
+
 	while (yylex() != 0);
+
+	fclose(yyin);
 	return 0;
 }
