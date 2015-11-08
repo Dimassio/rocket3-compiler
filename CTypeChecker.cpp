18d54e6ed34f62e3bcdcd03a006fa1c5513bfc8c
Dmitry Zvonarev <dmitry.zvonorev@phystech.edu>
2015-11-09 01:27:07 +0300

    + is for todo

--- CTypeChecker.cpp
+++ CTypeChecker.cpp
@@ -19,39 +19,17 @@ void CTypeChecker::visit( const CProgram* program )
 
 void CTypeChecker::visit( const CMainClass* mainClass )
 {
-	printIdentation();
-	std::cout << "class " << mainClass->ClassSymbol()->String() << " {" << std::endl;
-	++identation;
-	printIdentation();
-	std::cout << "public static void main(String[] " << mainClass->ArgSymbol()->String() << " ) { " << std::endl;
-	++identation;
 	mainClass->Statement()->Accept( this );
-	--identation;
-	printIdentation();
-	std::cout << "}" << std::endl;
-	--identation;
-	printIdentation();
-	std::cout << "}" << std::endl << std::endl;
 }
 
-void CTypeChecker::visit( const CClassDecl* classDecl )
+void CTypeChecker::visit( const CClassDecl* classDecl ) // + Æåíÿ
 {
-	printIdentation();
-	std::cout << "class " << classDecl->ClassSymbol()->String();
-	if( classDecl->ExtendedClassSymbol()->String() != "" ) {
-		std::cout << " extends " << classDecl->ExtendedClassSymbol()->String();
-	}
-	std::cout << " { " << std::endl;
-	++identation;
 	if( classDecl->VarDeclList() != 0 ) {
 		classDecl->VarDeclList()->Accept( this );
 	}
 	if( classDecl->MethodDeclList() != 0 ) {
 		classDecl->MethodDeclList()->Accept( this );
 	}
-	--identation;
-	printIdentation();
-	std::cout << "}" << std::endl << std::endl;
 }
 
 void CTypeChecker::visit( const CClassDeclList* classDeclList )
@@ -89,74 +67,65 @@ void CTypeChecker::visit( const CExp* exp )
 	}
 }
 
-void CTypeChecker::visit( const CExpMethodCall* expMethodCall )
+void CTypeChecker::visit( const CExpMethodCall* expMethodCall ) // +
 {
 	expMethodCall->Exp()->Accept( this );
-	std::cout << "." << expMethodCall->Symbol()->String() << "(";
 
 	expMethodCall->ExpList()->Accept( this );
-	std::cout << ")";
 }
 
-void CTypeChecker::visit( const CExpBinOperation* expBinOperation )
+void CTypeChecker::visit( const CExpBinOperation* expBinOperation ) // +
 {
 	expBinOperation->FirstExp()->Accept( this );
-	std::cout << " " << expBinOperation->ExpName() << " ";
 	expBinOperation->SecondExp()->Accept( this );
 }
 
-void CTypeChecker::visit( const CExpNewIntArray* expNewIntArray )
+void CTypeChecker::visit( const CExpNewIntArray* expNewIntArray ) // + new int[]
 {
-	std::cout << "new int[";
 	expNewIntArray->Exp()->Accept( this );
-	std::cout << "]";
 }
 
-void CTypeChecker::visit( const CExpNewCustomType* expNewCustomType )
+void CTypeChecker::visit( const CExpNewCustomType* expNewCustomType ) // + new Type()
 {
-	std::cout << "new" << expNewCustomType->Type() << "()";
+	
 }
 
-void CTypeChecker::visit( const CExpSquareBrackets* expSquareBrackets )
+void CTypeChecker::visit( const CExpSquareBrackets* expSquareBrackets ) // + first[]
 {
 	expSquareBrackets->FirstExp()->Accept( this );
-	std::cout << "[";
+
 	expSquareBrackets->SecondExp()->Accept( this );
-	std::cout << "]";
 }
 
 void CTypeChecker::visit( const CExpRoundBrackets* expRoundBrackets )
 {
-	std::cout << "(";
 	expRoundBrackets->Exp()->Accept( this );
-	std::cout << ")";
 }
 
-void CTypeChecker::visit( const CExpNot* expNot )
+void CTypeChecker::visit( const CExpNot* expNot ) // +
 {
-	std::cout << "!";
 	expNot->Exp()->Accept( this );
 }
 
-void CTypeChecker::visit( const CExpNumber* expNumber )
+void CTypeChecker::visit( const CExpNumber* expNumber ) // +
 {
-	std::cout << expNumber->Number();
+	
 }
 
-void CTypeChecker::visit( const CExpId* expId )
+void CTypeChecker::visit( const CExpId* expId ) //+
 {
-	std::cout << expId->Symbol()->String();
+	
 }
 
-void CTypeChecker::visit( const CExpSingle* expSingle )
+void CTypeChecker::visit( const CExpSingle* expSingle ) //+
 {
-	std::cout << expSingle->ExpName();
+	
 }
 
-void CTypeChecker::visit( const CExpLength* expLength )
+void CTypeChecker::visit( const CExpLength* expLength ) // +
 {
 	expLength->Exp()->Accept( this );
-	std::cout << ".Length";
+
 }
 
 void CTypeChecker::visit( const CExpList* expList )
@@ -171,12 +140,10 @@ void CTypeChecker::visit( const CExpList* expList )
 
 void CTypeChecker::visit( const CFormalList* formalList )
 {
-	std::string id = formalList->Symbol()->String();
 	if( !formalList->Type() && id == "" && !formalList->FormalRestList() ) {
 		return;
 	}
 	formalList->Type()->Accept( this );
-	std::cout << id;
 	if( formalList->FormalRestList() ) {
 		formalList->FormalRestList()->Accept( this );
 	}
@@ -184,29 +151,18 @@ void CTypeChecker::visit( const CFormalList* formalList )
 
 void CTypeChecker::visit( const CMethodDecl* methodDecl )
 {
-	printIdentation();
-	std::cout << "public ";
 	( methodDecl->Type() )->Accept( this );
-	std::cout << methodDecl->Symbol()->String() << "(";
 	( methodDecl->FormalList() )->Accept( this );
-	std::cout << ") {" << std::endl;
-	++identation;
 	if( methodDecl->VarDeclList() ) {
 		( methodDecl->VarDeclList() )->Accept( this );
 	}
 	if( methodDecl->StatementList() ) {
 		( methodDecl->StatementList() )->Accept( this );
 	}
-	printIdentation();
-	std::cout << "return ";
 	( methodDecl->Exp() )->Accept( this );
-	std::cout << ";" << std::endl;
-	--identation;
-	printIdentation();
-	std::cout << "}" << std::endl << std::endl;
 }
 
-void CTypeChecker::visit( const CMethodDeclList* methodDeclList )
+void CTypeChecker::visit( const CMethodDeclList* methodDeclList ) 
 {
 	if( methodDeclList->MethodDeclList() ) {
 		methodDeclList->MethodDeclList()->Accept( this );
@@ -217,57 +173,39 @@ void CTypeChecker::visit( const CMethodDeclList* methodDeclList )
 void CTypeChecker::visit( const CStatement* statement )
 {
 	if( statement->GetStatementType() == "BlockStatement" ) {
-		--identation;
-		printIdentation();
-		std::cout << "{" << std::endl;
-		++identation;
+
 		statement->Statements()->Accept( this );
-		--identation;
-		printIdentation();
-		std::cout << "}" << std::endl;
-		++identation;
-	} else if( statement->GetStatementType() == "IfStatement" ) {
-		printIdentation();
-		std::cout << "if (";
+
+	} else if( statement->GetStatementType() == "IfStatement" ) { // +
+
 		statement->FirstExpression()->Accept( this );
-		std::cout << ")" << std::endl;
-		++identation;
+
 		statement->FirstStatement()->Accept( this );
-		--identation;
-		printIdentation();
-		std::cout << "else" << std::endl;
-		++identation;
+
+	
 		statement->SecondStatement()->Accept( this );
-		std::cout << std::endl;
-		--identation;
-	} else if( statement->GetStatementType() == "WhileStatement" ) {
-		printIdentation();
-		std::cout << "while (";
+
+	} else if( statement->GetStatementType() == "WhileStatement" ) { // +
+	
+
 		statement->FirstExpression()->Accept( this );
-		std::cout << ")" << std::endl;
-		++identation;
+
 		statement->FirstStatement()->Accept( this );
-		std::cout << std::endl;
-		--identation;
-	} else if( statement->GetStatementType() == "PrintlnStatement" ) {
-		printIdentation();
-		std::cout << "System.out.println(";
+
+	} else if( statement->GetStatementType() == "PrintlnStatement" ) { // +
 		statement->FirstExpression()->Accept( this );
-		std::cout << ");" << std::endl;
-	} else if( statement->GetStatementType() == "AssignStatement" ) {
-		printIdentation();
+
+	} else if( statement->GetStatementType() == "AssignStatement" ) { // +
+
 		std::cout << statement->Symbol()->String();
-		std::cout << " = ";
+
 		statement->FirstExpression()->Accept( this );
-		std::cout << ";" << std::endl;
-	} else if( statement->GetStatementType() == "ArrayAssignStatement" ) {
-		printIdentation();
-		std::cout << statement->Symbol()->String();
-		std::cout << "[";
+
+	} else if( statement->GetStatementType() == "ArrayAssignStatement" ) { // +
+
 		statement->FirstExpression()->Accept( this );
-		std::cout << "] = ";
+		
 		statement->SecondExpression()->Accept( this );
-		std::cout << ";" << std::endl;
 	}
 }
 
@@ -281,14 +219,12 @@ void CTypeChecker::visit( const CStatementList* statementList )
 
 void CTypeChecker::visit( const CType* type )
 {
-	std::cout << type->GetTypeName() << " ";
+
 }
 
 void CTypeChecker::visit( const CVarDecl* varDecl )
 {
-	printIdentation();
 	( varDecl->Type() )->Accept( this );
-	std::cout << varDecl->Symbol()->String() << ";" << std::endl;
 }
 
 void CTypeChecker::visit( const CVarDeclList* varDeclList )
@@ -309,9 +245,7 @@ void CTypeChecker::visit( const CFormalRestList* formalRestList )
 
 void CTypeChecker::visit( const CFormalRest* formalRest )
 {
-	std::cout << ", ";
 	formalRest->Type()->Accept( this );
-	std::cout << formalRest->Symbol()->String();
 }
 
 void CTypeChecker::visit( const CExpRestList* expRestList )
@@ -326,7 +260,6 @@ void CTypeChecker::visit( const CExpRestList* expRestList )
 
 void CTypeChecker::visit( const CExpRest* expRest )
 {
-	std::cout << ", ";
 	( expRest->Exp() )->Accept( this );
 }
 
